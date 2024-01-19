1476c9912b90a3b6f7774517744ac03b1f4c89e3
michaelmc9666 <88546900+michaelmc9666@users.noreply.github.com>
2024-01-19 11:36:51 -0700

    Added MySQL database progress

---
+++ Dump20231122/pokedex_evolution.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
+--
+-- Host: localhost    Database: pokedex
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `evolution`
+--
+
+DROP TABLE IF EXISTS `evolution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `evolution` (
+  `evolution_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `base_pokemon_id` int NOT NULL,
+  `evolved_pokemon_id` int NOT NULL,
+  `evolution_method` text NOT NULL,
+  PRIMARY KEY (`evolution_id`),
+  KEY `base_pokemon_id` (`base_pokemon_id`),
+  KEY `evolved_pokemon_id` (`evolved_pokemon_id`),
+  CONSTRAINT `evolution_ibfk_1` FOREIGN KEY (`base_pokemon_id`) REFERENCES `pokemon` (`pokemon_id`),
+  CONSTRAINT `evolution_ibfk_2` FOREIGN KEY (`evolved_pokemon_id`) REFERENCES `pokemon` (`pokemon_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evolution`
+--
+
+LOCK TABLES `evolution` WRITE;
+/*!40000 ALTER TABLE `evolution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evolution` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-22 11:29:53
