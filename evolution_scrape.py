c4ed418c5978975100a310c4e2db8bc8c7264b71
michaelmc9666 <88546900+michaelmc9666@users.noreply.github.com>
2024-02-16 18:03:44 -0700

    updated evolution_scrape.py. rain into some issues. fixing soon.

--- evolution_scrape.py
+++ evolution_scrape.py
@@ -2,10 +2,36 @@ from bs4 import BeautifulSoup
 import requests
 
 def scrape_evolution_data(block):
-    base = block.find('span', class_='ent-name')
-    base_pokemon_id = base.text.split("#")[1]
+    # initialize variables
+    base_id, evolved_id, method = None, None, "Unknown"
 
-    evolved = block.find('small')
+    # extracting the base pokemon details
+    base = block.find('span', class_='ent-name')
+    print("Base element: ", base)
+
+    if base:
+        base_id = base.text.split("#")[1]
+    else:
+        print("Base element not found in block.")
+
+    # extracting the evolved pokemon details
+    evolved = block.find('a', class_='ent-name')
+    print("Evolved block element: ")
+    if evolved and "#" in evolved.text:
+        evolved_id = evolved.text.split('#')[1]
+    else:
+        print("Evolved id not found in block.")
+
+    # extracting evolution method. in same div
+    method_container = block.find('div', class_='infocard-arrow')
+    if method_container:
+        method = block.find('div', class_='infocard-arrow').find_next_sibling(text=True).strip()
+    else:
+        print("Evolved method not found")
+
+    return {'Base ID': base_id,
+            'Evolved ID': evolved_id,
+            'Method': method}
 
 # url of page I am scraping
 url = 'https://pokemondb.net/evolution'
@@ -22,7 +48,9 @@ evolution_blocks = soup.find_all('div', class_='infocard')
 evolution_data = []
 
 for evolution_block in evolution_blocks:
-    evolution_data.append(evolution_block)
+    evolution_data.append(scrape_evolution_data(evolution_block))
+
+print(evolution_data)
 
 
 
